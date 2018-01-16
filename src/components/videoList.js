b836ba218fa34a051173fc7f608310f8dee1f761
Ioannis Ioakeimidis <jioakim@gmail.com>
2018-01-17 00:18:55 +0200

    basic requirements

--- src/components/videoList.js
+++ src/components/videoList.js
@@ -2,8 +2,8 @@ angular.module('video-player')
 .component('videoList', {
   template: `
   <ul class="video-list">
-    Jay
-    <video-list-entry ng-repeat="video in $ctrl.videos" video="video" on-click="$ctrl.onClick"><h5>Jay</h5></video-list-entry>
+    Jay jhgjhgjhgjhgjhg
+    <video-list-entry ng-repeat="video in $ctrl.videos" video="video" on-click="$ctrl.onClick"></video-list-entry>
   </ul>
   `,
   bindings: {
